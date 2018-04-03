ec05292551d9b09ea25d49f3dca8e99bcb6353fb
Avni Shivkumar <avnishivkumar@Avnis-MBP.wireless.newschool.edu>
2018-04-03 12:47:30 -0400

    [some message]

--- studio/main.js
+++ studio/main.js
@@ -1,42 +1,28 @@
-var m = document.getElementsByClassName('move');
-m.addEventListener('mousedown', mouseDown, false);
-window.addEventListener('mouseup', mouseUp, false);
-
-function mouseUp() {
-    window.removeEventListener('mousemove', move, true);
-}
-
-function mouseDown(e) {
-    window.addEventListener('mousemove', move, true);
-}
-
-function move(e) {
-    m.style.top = e.clientY + 'px';
-    m.style.left = e.clientX + 'px';
-};
 
 ////////
 
 $(document).bind('mousemove', function(e){
   console.log(e.pageX);
     $('#follower').css({
-       left:  e.pageX,
-       top:   e.pageY,
+      left:  e.pageX +560,
+      top:   e.pageY+200,
+
     });
 });
 
 $(document).bind('mousemove', function(e){
   console.log(e.pageX);
     $('#follower2').css({
-       left:  e.pageX + 100,
-       top:   e.pageY
+       left:  e.pageX - 600,
+       top:   e.pageY -750,
+
     });
 
     $(document).bind('mousemove', function(e){
   console.log(e.pageX);
     $('#follower3').css({
-       left:  e.pageX + 0,
-       top:   e.pageY+80
+      left:  e.pageX +100,
+      top:   e.pageY -30,
     });
 });
 
@@ -52,7 +38,72 @@ $(document).bind('mousemove', function(e){
 $(document).bind('mousemove', function(e){
   console.log(e.pageX);
     $('#follower5').css({
-       left:  e.pageX + 100,
-       top:   e.pageY+80
+       left:  e.pageX + 1000,
+       top:   e.pageY+1000,
     });
 });
+
+
+$(document).on('mousemove', function(e) {
+    $('#follower5').css({
+      left:  e.pageX -300,
+      top:   e.pageY -100,
+        width: '+=5px',
+        height: '+=5px'
+    });
+});
+
+$(document).on('mousemove', function(e) {
+    $('#follower6').css({
+      left:  e.pageX -400,
+      top:   e.pageY+200,
+        width: '+=5px',
+        height: '+=5px'
+    });
+});
+
+$(document).on('mousemove', function(e) {
+    $('#follower7').css({
+      left:  e.pageX + 400,
+      top:   e.pageY -300,
+        width: '+=5px',
+        height: '+=5px'
+    });
+});
+
+$(document).on('mousemove', function(e) {
+    $('#follower8').css({
+      left:  e.pageX -400,
+      top:   e.pageY -500,
+        width: '+=5px',
+        height: '+=5px'
+    });
+});
+$(document).on('mousemove', function(e) {
+    $('#follower9').css({
+      left:  e.pageX +400,
+      top:   e.pageY -300,
+        width: '+=5px',
+        height: '+=5px'
+    });
+});
+$(document).on('mousemove', function(e) {
+    $('#follower10').css({
+      left:  e.pageX +100,
+      top:   e.pageY -600,
+        width: '+=5px',
+        height: '+=5px'
+    });
+});
+
+$('.move').click(function(){
+  if($(this).hasClass('move')) {
+    $(this).addClass('movebig');
+  } else {
+$(this).addClass('move');
+};
+});
+
+
+
+//
